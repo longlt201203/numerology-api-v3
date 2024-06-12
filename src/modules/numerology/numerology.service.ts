f1d74460416415542d25e45824bccec9bab09142
longlt201203 <longlt201203@nexondv.com>
2024-06-12 17:52:37 +0700

    feat: 🎸 update

--- src/modules/numerology/numerology.service.ts
+++ src/modules/numerology/numerology.service.ts
@@ -11,6 +11,11 @@ export class NumerologyService {
         @InjectRepository(NumerologyEntry)
         private readonly numerologyEntryRepo: Repository<NumerologyEntry>
     ) { }
+    private readonly charMap = {
+        a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 1,
+        k: 2, l: 3, m: 4, n: 5, o: 6, p: 7, q: 8, r: 9, s: 1, t: 2,
+        u: 3, v: 4, w: 5, x: 6, y: 7, z: 8
+    } as const;
 
     getMany() {
         return this.numerologyEntryRepo.find({ order: { number: "ASC" } });
